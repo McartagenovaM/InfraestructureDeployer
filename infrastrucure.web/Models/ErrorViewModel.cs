cbcec820773aadd6cb20aa5cc0b5daff9806c597
Miguel <Miguel@PRODESK-DEV>
2025-09-30 10:44:47 -0500

    Add project files.

---
+++ infrastrucure.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace infrastrucure.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
